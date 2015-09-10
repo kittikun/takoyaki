6fa2020b2d08d86afbef45933734311ce151931c
kittikun <me@kittikun.jp>
2015-09-11 05:31:22 +0900

    Post-build as powershell script

---
+++ build/postbuild.ps1
@@ -0,0 +1,8 @@
+param (
+    [string]$src = $(throw "-src is required."),
+    [string]$dst = $(throw "-dst is required.")
+)
+
+Write-Host "Running post-build..."
+Write-Host "Copying files from $src to $dst"
+Copy-Item $src $dst -recurse -force

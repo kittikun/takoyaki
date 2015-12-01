f52be12ee815033bc73dd23b6b2a24c2ef0c68f2
kittikun <me@kittikun.jp>
2015-12-02 01:45:12 +0900

    Remove WinRT test app

--- build/prebuild.ps1
+++ build/prebuild.ps1
@@ -12,12 +12,12 @@ Write-Host -foregroundcolor "Yellow" "Generating boost headers.."
 .\b2 headers -d0
 
 if ($conf -eq "Debug") {
-    Write-Host -foregroundcolor "Yellow" "Building boost for WinRT app.."
+    Write-Host -foregroundcolor "Yellow" "Building boost for Universal Windows.."
     ./b2 -d0 address-model=64 toolset=msvc-14.0 variant=debug link=static threading=multi runtime-link=shared windows-api=desktop --stagedir=winrt --with-log
     Write-Host -foregroundcolor "Yellow" "Building boost for Win32 unit tests.."
     ./b2 -d0 address-model=64 toolset=msvc-14.0 variant=debug link=static threading=multi runtime-link=shared --stagedir=win --with-log --with-program_options --with-filesystem
 } elseif ($conf -eq "Release") {
-    Write-Host -foregroundcolor "Yellow" "Building boost for WinRT app.."
+    Write-Host -foregroundcolor "Yellow" "Building boost for Universal Windows.."
     ./b2 -d0 address-model=64 toolset=msvc-14.0 variant=release link=static threading=multi runtime-link=shared windows-api=desktop --stagedir=winrt --with-thread --with-date_time
     Write-Host -foregroundcolor "Yellow" "Building boost for Win32 unit tests.."
     ./b2 -d0 address-model=64 toolset=msvc-14.0 variant=release link=static threading=multi runtime-link=shared --stagedir=win --with-program_options --with-filesystem --with-thread --with-date_time
